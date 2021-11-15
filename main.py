21d8c9098759a56f7079071586512ddb0725bed4
bitCoinIsGod <2bh0104@gmail.com>
2021-11-15 14:32:46 +0900

    파이참

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("gh ")
+import sys
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv)
+
+btn = QPushButton("Hello")    # 버튼 객체 생성
+btn.show()
+
+app.exec_()                          # 이벤트 루프 생성
